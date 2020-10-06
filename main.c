c82ae5502f9220ea9849428358b59a6cd1334bce
pmankyu <pmankyu@gmail.com>
2020-10-07 00:08:29 +0900

    cardInfo 구조체 선언 및 초기화, 함수원형 추가

--- main.c
+++ main.c
@@ -3,6 +3,26 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+struct CardInfo {
+	int isAvailable;
+    int pin;
+    int account;
+	int money;
+};
+
+int addCardInfo(){
+	return 0;
+}
+
+int delCardInfo(){
+	return 0;
+}
+
+int insertCard(){
+	return 0;
+}
+
 int main(int argc, char *argv[]) {
+	struct CardInfo card[3] = {0};
 	return 0;
 }
