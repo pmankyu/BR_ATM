28cbd26b2f79c847264d6092a6c82a8bc7ba655a
pmankyu <pmankyu@gmail.com>
2020-10-07 00:18:50 +0900

    addCardInfo 구현

--- main.c
+++ main.c
@@ -3,14 +3,27 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int card_num;
+
 struct CardInfo {
-	int isAvailable;
     int pin;
-    int account;
+    char account[10];
 	int money;
-};
+}card[3];
 
 int addCardInfo(){
+	if(card_num > 2){
+		printf("The storage limit has been exceeded.\n");
+		return 1;
+	}
+
+	printf("Input your Card infomation\n");
+	printf("pin account\n");
+	printf("ex) pin = 1234, account = abc\n");
+	printf("input : 1234 abc\n");
+
+	scanf("%d %s", &card[card_num].pin, &card[card_num].account);
+	card_num++;
 	return 0;
 }
 
@@ -23,6 +36,5 @@ int insertCard(){
 }
 
 int main(int argc, char *argv[]) {
-	struct CardInfo card[3] = {0};
 	return 0;
 }
